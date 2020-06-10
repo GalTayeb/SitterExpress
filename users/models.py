0ad4dc21fbafbe22b7e3cf5472893a5544ee8ea1
galtayeb <galtayeb@gmail.com>
2020-06-10 15:22:07 +0300

    New Update

--- users/models.py
+++ users/models.py
@@ -10,11 +10,14 @@ class Profile(models.Model):
         ('M', 'Male'),
         ('F', 'Female'),
     )
-    gender = models.CharField(blank=True, max_length=10, choices=GENDER_CHOICES)
-    age = models.CharField(blank=True, max_length=3)
-    id_number = models.CharField(blank=True, max_length=9)
-    phone_number = models.CharField(blank=True, max_length=10)
+    gender = models.CharField(default='', max_length=10, choices=GENDER_CHOICES)
+    age = models.CharField(default='', max_length=3)
+    id_number = models.CharField(default='', max_length=9)
+    phone_number = models.CharField(default='', max_length=10)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    one_to_five_choices = zip(range(1, 5 + 1), range(1, 5 + 1))
+    rating = models.PositiveSmallIntegerField(default=0, choices=one_to_five_choices)
 
     def __str__(self):
         return f'{self.user.username} Profile'
