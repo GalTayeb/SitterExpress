cd1a499f789170b2c394a915a3e3c28856fe69fd
galtayeb <galtayeb@gmail.com>
2020-06-22 02:40:35 +0300

    New Update

--- babySitter/urls.py
+++ babySitter/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.welcome, name='babySitter'),
+    path('', views.welcome, name='babySitter-welcome'),
     path('home/', views.home, name='babySitter-home'),
     path('about/', views.about, name='babySitter-about'),
     path('orders/', views.orders, name='babySitter-orders'),
