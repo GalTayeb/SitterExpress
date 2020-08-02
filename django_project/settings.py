e12b5dc6ff9ab85e07ec39959b05f15ee9cdf64f
galtayeb <galtayeb@gmail.com>
2020-08-02 16:33:02 +0300

    New Update

--- django_project/settings.py
+++ django_project/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -95,16 +95,16 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'database1',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345678',
-#         'HOST': 'database1.cqav2vafoalq.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database1',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'database1.cqav2vafoalq.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
