7eb3a59aa013e1de5729b2c5f23e89bdc8fc6d38
Chen Niego <chenniego@gmail.com>
2020-08-17 16:33:33 +0300

    Update

--- babySitter/models.py
+++ babySitter/models.py
@@ -5,3 +5,6 @@ class Order(models.Model):
     date = models.CharField(default='', max_length=10)
     contact = models.CharField(default='', max_length=100)
     status = models.BooleanField(default=False)
+
+    # User | Date | ContactDetails| Status
+    # Order.objects.filter(user=user).filter(status=0)
--- babySitter/views.py
+++ babySitter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from babySitter.models import Order
 from users.models import ModelBabysitter
+import json
+from django.http import HttpResponse
+from django.contrib.gis.geoip2 import GeoIP2
 
 
 def welcome(request):
@@ -9,12 +12,21 @@ def welcome(request):
 
 def home(request):
     if request.user.is_authenticated:
-        if request.user.is_superuser:
-            return redirect('/admin/')
         if request.user.is_babysitter:
-            return redirect('babySitter-order')
+            return redirect('babySitter-about')
         elif request.user.is_parent:
+            if request.method == 'POST':
+                price = request.POST.get('price') #salary_per_hour
+                kids = request.POST.get('kids') #max_kids
+                rating = request.POST.get('rating') #rating
+                radius = request.POST.get('radius')
+                results = ModelBabysitter.objects.filter(salary_per_hour__lte=int(price)).filter(max_kids__gte=int(kids)).filter(rating__gte=float(rating))
+                return render(request, 'babySitter/details.html', {"users": results})
             return render(request, 'babySitter/home.html')
+#
+# def babysitterdetails(request):
+#     # orders = order.objects.filter(user = user)
+#     return render(request, 'page',{"orders":orders})
 
 
 def about(request):
@@ -37,3 +49,32 @@ def details(request):
 
 def choice(request):
     return render(request, 'babySitter/choice.html')
+
+
+# def is_online(request):
+#     isonline = request.GET.get('isonline')
+#     isready = request.GET.get('isready')
+#     userid = request.GET.get('userid')
+#
+#     res = {"userid": userid}
+#     return HttpResponse(json.dumps(res))
+
+
+# def get_locations(request):
+#     lat = request.GET.get('lat')
+#     lon = request.GET.get('lon')
+#     radius = request.GET.get('radius')
+#     db_res = db_query(lat, lon, radius)
+#
+#     res = {
+#         "lon": db_res.lon,
+#         "lat": db_res.lat,
+#         "id": db_res.id,
+#     }
+#     return HttpResponse(json.dumps(res))
+
+
+# def search(request):
+#     kids
+#     rating
+#     price
